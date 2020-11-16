17517c8f2efbea9595bfb6e014930cc0332032b3
kellim521 <63273771+kellim521@users.noreply.github.com>
2020-11-16 15:34:27 -0600

    Updated to include correct thermal diffusivity

--- TESmodel.py
+++ TESmodel.py
@@ -1,28 +1,13 @@
-import math
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 19:15:31 2020
+
+@author: kelli
+"""
+
 import numpy as np
-
-cod = 0.0002  # Thermal diffusivity
-peit = 2000
-hotT = 500
-
-
-class P2S_HX:
-    def __init__(self, m_dot_hot, c_p_hot, T_hot_in, m_dot_cold, c_p_cold, T_hot_out, U, A, HE_Type):
-        C_hot = m_dot_hot * c_p_hot
-        C_cold = m_dot_cold * c_p_cold
-        C_min = min(C_hot, C_cold)
-        C_max = max(C_hot, C_cold)
-        C_r = C_min / C_max
-        NTU = U * A / C_min
-        epsilon = effectiveness(NTU, C_r, HE_Type)
-        Q = C_hot * (T_hot_in - T_hot_out)
-        Q_max = Q / epsilon
-        T_cold_in = T_hot_in - Q_max / C_min
-        T_cold_out = T_cold_in + Q / C_cold
-
-        self.epsilon = epsilon
-        self.T_cold_in = T_cold_in
-        self.T_cold_out = T_cold_out
+import math
+from thermo.chemical import Chemical
 
 
 class tmodel:
@@ -40,36 +25,16 @@ class tmodel:
 
         ths = np.zeros(p)  # Storage bed temperature
 
-        # COD: Thermal Dispersion factor
-
         pei = peit  # conjugate heat transfer-stanton number
-        # peo = 0.0  # Wall heat loss stanton number
         pe = 0.0003  # Peclet number
-
         pd = np.zeros(p)  # diffusion part
-
         pdr = np.zeros(p)  # diffusion part-recovery
-
-        # pdp = np.zeros(p)
-
-        # pdpr = np.zeros(p)
-
-        # rhod = np.zeros(p)
-
-        # red = np.zeros(p)
-
         thr = np.ones(p)
-
         tdr = np.zeros(p)
-
         td = np.zeros(p)
-
         dth = np.zeros(p)
-
         thrs = np.ones(p)
-
         thss = np.zeros(70000)
-
         thrss = np.zeros(70000)
 
         """ dp = 0.002
@@ -127,73 +92,19 @@ class tmodel:
         self.retime = u
         print('u = ', u)
         print('finished tmodel')
+        
+if __name__ == "__main__":
+    
+    hotT = 500
 
+    alumina = Chemical('PubChem=14769', T = hotT)
+    sodiumNitrate = Chemical('sodium nitrate', T = hotT)
 
-def effectiveness(NTU, C_r, HE_Type):
-    if C_r == 0:
-        epsilon = 1 - math.exp(-NTU)
-        return epsilon
+    k = alumina.kl*.65 + sodiumNitrate.kl*.35
+    rho = alumina.rho*.65 + sodiumNitrate.rho*.35
+    Cp = alumina.Cp*.65 + sodiumNitrate.Cp*.35
 
-    if HE_Type == 'Parallel Flow':
-        epsilon = (1 - math.exp(-NTU * (1 + C_r))) / (1 + C_r)
-    elif HE_Type == 'Counter Flow':
-        if C_r == 1:
-            epsilon = NTU/(1+NTU)
-        else:
-            epsilon = (1-math.exp(-NTU*(1-C_r)))/(1-C_r*math.exp(-NTU*(1-C_r)))
-    elif HE_Type == 'Double Shell Pass':
-        N = 2.0
-        NTUN = NTU/N
-        epsilon1 = 2/(1+C_r+math.sqrt(1+C_r**2)*(1+math.exp(-NTUN*math.sqrt(1+C_r**2)))/(1+math.exp(-NTUN*math.sqrt(1+C_r**2))))
-        epsilon = (((1-epsilon1*C_r)/(1-epsilon1))**(N-1)) / (((1-epsilon1*C_r)/(1-epsilon1))*(N-C_r))
-
-    return epsilon
-
-
-def exer(ratio, Ts_in, Th_in, material):
-
-    thsm = tmodel(cod, peit, hotT)
-    x = 0
-    k = 0.0001
-
-    for i in range(thsm.retime):
-        x = x + (thsm.stout[i] + (1 / ratio) * math.log((thsm.stout[i]) * ratio + 1)) * k  # exergy integration
-
-    y = x / (1.5 * k)  # fractional exergy recovery
-
-    if material == 'Alumina':
-        exev = y / thsm.retime
-    elif material == 'Molten_salt':
-        exev = 0.9 * Ts_in / Th_in
-
-    exep = exev-0.06
-
-    return exep
-
-
-def eco(exer_value, material, block_eff, Tc_out, Tc_in):
-    pass
-
-
-# Test case
-cp_h = 1  # KJ/kg-K
-m_h = 20  # kg/s
-Th_in = 550+273  # K
-Th_out = 450+273  # K
-cp_c = 1  # KJ/kg-K
-m_c = 15  # kg/s
-U = 2  # kW/m^2-K
-A = 20  # m2
-ratio = Th_in/293  # Ratio of T_hot/T_ambient in K
-material = 'Molten_salt'
-
-p2s_hx = P2S_HX(m_h, cp_h, Th_in, m_c, cp_c, Th_out, U, A, 'Double Shell Pass')
-ep = p2s_hx.epsilon
-Tc_in = p2s_hx.T_cold_in
-Tc_out = p2s_hx.T_cold_out
-
-Ts_in = Tc_out
-exer_value = exer(ratio, Ts_in, Th_in, material)
-
-print("Tc_in =  ", Tc_in, " Tc_out = ", Tc_out)
-# st_cost = eco(exer_value, material, 0.4, Tc_out, Tc_in)  # Storage cost in $/KWhr
+    cod = (k/(rho*Cp))    #*1000
+    
+    peit = 2000
+    tmodel(cod, peit, hotT)

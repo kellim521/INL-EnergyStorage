4ba5a41e4f6c58f9603e6c62d576568fe371efaa
kellim521 <63273771+kellim521@users.noreply.github.com>
2020-12-06 16:13:12 -0600

    update MassFlowRateControls.py

--- MassFlowRateControls.py
+++ MassFlowRateControls.py
@@ -4,36 +4,7 @@ Mass Flow Rate Control
 
 """
 import csv
-import numpy as np
-import math
-
-class tank:
-        
-    def __init__(self, currentVolume, totalVolume):
-        #density of solar salt (kg/m^3)
-        self.rhoSalt = 1804
-        self.volume_salt = currentVolume
-        self.volume_tank = totalVolume
-        self.timeOverflow = math.inf
-        self.timeEmpty = math.inf
-    
-    def VolChange(self, mOut, mIn): 
-        vOut = mOut/self.rhoSalt #m^3/s
-        vIn = mIn/self.rhoSalt #m^3/s
-        vStore = vIn - vOut #m^3/s
-        
-        if vStore > 0:
-            self.timeOverflow = (self.volume_tank - self.volume_salt)/vStore
-
-            # add the volume of salt that will accumulate in 10 mins
-            self.volume_salt += vStore*600
-        
-        elif vStore < 0:
-            self.timeEmpty = self.volume_salt/-vStore
-            
-            # remove the volume of salt that will accumulate in 10 mins
-            self.volume_salt += vStore*600
- 
+import numpy as np 
 
 # From Steam Generator, into cold side of TES (kg/s)
 mdot_1 = 1500
@@ -51,12 +22,7 @@ mdot_4 = 0
 mdot_5 = 0
 
 # From hot side to Steam Generator
-mdot_6 = 0
-
-# storage volumes for hot and cold side (m^3)
-# currently just test values
-hotTank = tank(1000, 6000)
-coldTank = tank(3000, 6000)
+mdot_6 = 1500
 
 # import mass flow rates
 solarData = np.empty(0)
@@ -76,32 +42,30 @@ with open('Reactor.csv', newline='') as Reactor:
 hotTankVolumes = np.zeros(145)
 coldtankVolumes = np.zeros(145)
 
-# loops through a day in 10 min intervals based on solar and reactor data
-for i in range(145):
+solar = solarData[0]        
+mdot_3 = float(solar['mdot_s'])
+mdot_2 = mdot_3
+
+reactor = reactorData[0]
+mdot_5 = float(reactor['mdot_r'])
+mdot_4 = mdot_5
+
+
+# loops through a day in 10 min intervals based on solar data to determine 
+# ideal reactor mass flow rate
+
+for i in range(0,145):
+    idealReactorData = [0,0]
+
     solar = solarData[i]        
     mdot_3 = float(solar['mdot_s'])
     mdot_2 = mdot_3
     
-    reactor = reactorData[i]
-    mdot_5 = float(reactor['mdot_r'])
-    mdot_4 = mdot_5
-    
-    mdot_1 = 1500
-    mdot_6 = mdot_3 + mdot_5
-            
-    if mdot_6 != mdot_1:
-        
-        # adjust flow out of hot side to match desired flow for steam generator
-        mdot_6 = mdot_1
-        
-        # change volume of hot side
-        hotTank.VolChange(mdot_6, mdot_3+mdot_5)
-            
-        # change volume of cold side
-        coldTank.VolChange(mdot_2+mdot_4, mdot_1)
-        
-        hotTankVolumes[i] = hotTank.volume_salt
-        coldtankVolumes[i] = coldTank.volume_salt
+    idealReactorData[1] = mdot_6 - mdot_3
+    idealReactorData[0] = i+1
+    with open('IdealReactor.csv', 'a', newline='') as Reactor:
+        writer = csv.writer(Reactor)
+        writer.writerow(idealReactorData)
     
             
 
